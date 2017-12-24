3721c65be5a59a80f232c494f87640ed55edabb4
Benjamin Müller <bm@netinventors.de>
2017-12-24 08:29:00 +0100

    refactoring fields

--- Resources/views/backend/neti_tags/extensions/view/base/attribute/article/field.js
+++ Resources/views/backend/neti_tags/extensions/view/base/attribute/article/field.js
@@ -1,77 +1,7 @@
 //{namespace name="plugins/neti_prepaid/backend/article"}
 //{block name="backend/base/attribute/form" append}
 Ext.define('Shopware.apps.NetiTagsExtensions.view.base.attribute.article.Field', {
-    'extend': 'Ext.form.FieldContainer',
-    'alias': 'widget.neti_tags_article_attribute',
-    'layout': 'fit',
-    'mixins': {
-        'field': 'Ext.form.field.Field'
-    },
-    'value': null,
-    'initComponent': function () {
-        var me = this;
-
-        me.items = [
-            me.getGrid()
-        ];
-
-        me.callParent(arguments);
-    },
-
-    'getGrid': function () {
-        var me = this;
-
-        return me.grid || me.createGrid();
-    },
-
-    'createGrid': function () {
-        var me = this;
-
-        me.grid = Ext.create('Shopware.form.field.Grid', {
-            'store': me.store,
-            'searchStore': me.searchStore
-        });
-
-        return me.grid;
-    },
-
-    'setValue': function (value) {
-        var me = this;
-
-        me.grid.setValue(value);
-    },
-
-    'isValid': function () {
-        var me = this;
-
-        return me.grid.isValid();
-    },
-
-    'getValue': function () {
-        var me = this;
-
-        return me.grid.getValue();
-    },
-
-    'getSubmitValue': function () {
-        var me = this;
-
-        return me.getValue();
-    },
-
-    'getSubmitData': function () {
-        var me = this,
-            result = {};
-
-        result[me.getName()] = me.getValue();
-
-        return result;
-    },
-
-    'getModelData': function () {
-        var me = this;
-
-        return me.getValue();
-    }
+    'extend': 'Shopware.apps.NetiTagsExtensions.view.base.attribute.tags.Field',
+    'alias': 'widget.neti_tags_article_attribute'
 });
 //{/block}
