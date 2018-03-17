b02800db070ae40660ea692593db8cd0b3912eaa
Benjamin Müller <bm@netinventors.de>
2018-03-17 08:17:46 +0100

    #28251 // workaround of js error with the attribute grid plugin

--- Subscriber/Backend.php
+++ Subscriber/Backend.php
@@ -30,7 +30,7 @@ class Backend implements SubscriberInterface
     /**
      * Backend constructor.
      *
-     * @param string  $pluginDir
+     * @param string $pluginDir
      */
     public function __construct(
         $pluginDir
@@ -44,8 +44,43 @@ class Backend implements SubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'Enlight_Controller_Action_PostDispatch' => array('onPostDispatch', 10),
+            'Enlight_Controller_Action_PostDispatch'                  => array('onPostDispatch', 10),
+            'Enlight_Controller_Action_PostDispatch_Backend_Customer' => 'onPostDispatchCustomerStream',
         );
+    }
+
+    /**
+     * @param Enlight_Event_EventArgs $args
+     */
+    public function onPostDispatchCustomerStream(\Enlight_Event_EventArgs $args)
+    {
+        /**
+         * @var \Shopware_Controllers_Backend_CustomerStream $subject
+         * @var \Enlight_Controller_Request_RequestHttp      $request
+         * @var \Enlight_Controller_Response_ResponseHttp    $response
+         */
+        $subject  = $args->getSubject();
+        $request  = $args->get('subject')->Request();
+        $response = $args->get('subject')->Response();
+        if (! $request->isDispatched() || $response->isException()) {
+            return;
+        }
+
+        $module = $request->getModuleName();
+        if ('backend' !== $module) {
+            return;
+        }
+
+        /**
+         * @var \Enlight_View_Default $view
+         */
+        $view = $subject->View();
+
+        $view->addTemplateDir(
+            $this->pluginDir . '/Resources/views/'
+        );
+
+        $view->extendsTemplate('backend/neti_tags/extensions/view/customer/customer_stream/listing.js');
     }
 
     /**
