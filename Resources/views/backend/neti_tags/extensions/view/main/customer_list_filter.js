9fd4d9eb469c175b5e7fe6888798b129f2036304
Benjamin Müller <bm@netinventors.de>
2018-04-05 22:18:30 +0200

    #28761 // added tags to customer list filter

---
+++ Resources/views/backend/neti_tags/extensions/view/main/customer_list_filter.js
@@ -0,0 +1,22 @@
+//{namespace name="plugins/neti_tags/backend/detail/relations/customer"}
+// {block name="backend/customer/view/main/customer_list_filter"}
+//{$smarty.block.parent}
+Ext.override(Shopware.apps.Customer.view.main.CustomerListFilter, {
+    'configure': function() {
+        var me = this,
+            configuration = me.callParent(arguments),
+            factory = Ext.create('Shopware.attribute.SelectionFactory'),
+            tagsStore = factory.createEntitySearchStore("NetiTags\\Models\\Tag");
+
+        configuration.fields.tags = {
+            'fieldLabel': '{s name="customer_list_filter_field_label"}Tags{/s}',
+            'xtype': 'combobox',
+            'displayField': 'title',
+            'valueField': 'id',
+            'store': tagsStore
+        };
+
+        return configuration;
+    }
+});
+//{/block}
---
+++ Resources/views/backend/neti_tags/extensions/view/model/customer/quick_view.js
@@ -0,0 +1,4 @@
+// {block name="backend/customer/model/quick_view/fields"}
+//{$smarty.block.parent}
+{ name: 'tags', type: 'string' },
+//{/block}
