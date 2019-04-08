db26b8973b6aeca0baad554fe94cf10a9402b539
Sascha Brüggenolte <sb@netinventors.de>
2019-04-08 19:20:06 +0200

    Hotfix the caching issue

--- Service/TagsCache.php
+++ Service/TagsCache.php
@@ -7,6 +7,7 @@
 
 namespace NetiTags\Service;
 
+use Doctrine\ORM\AbstractQuery;
 use NetiTags\Models\Tag;
 use NetiTags\Service\Tag\RelationCollectorInterface;
 use Shopware\Components\Model\ModelManager;
@@ -62,13 +63,18 @@ class TagsCache
             return [];
         }
 
+        // TODO: Warmup runs only once per relation, disregarding the IDs. This can be heavily improved.
         if (!isset(self::$tagsCache[$relation])) {
-            $this->warmupTagsCache($ids, $relation);
+            // $this->warmupTagsCache($ids, $relation);
+            $this->warmupTagsCacheHotfix($ids, $relation);
         }
 
         $relationCache = &self::$tagsCache[$relation];
 
         $return = [];
+        // Previously the keys in the $relationCache variable have been incremental, so the isset made no sense.
+        // This is why we have adjusted the warmupTagsCache function.
+        // TODO: Move this into warmup or find a different solution, so that not every single ID is being fetched seperately.
         foreach ($ids as $id) {
             if (!isset($relationCache[$id])) {
                 $relationCache[$id] = $this->relationService->getTags($relation, $id);
@@ -106,5 +112,38 @@ class TagsCache
             )->setParameter('ids', $ids);
 
         self::$tagsCache[$relation] = \array_merge(self::$tagsCache[$relation], $qbr->getQuery()->getArrayResult());
+    }
+
+    public function warmupTagsCacheHotfix(array $ids, $relation)
+    {
+        if (\count($ids) < 1) {
+            return;
+        }
+
+        if (!isset(self::$tagsCache[$relation])) {
+            self::$tagsCache[$relation] = [];
+        }
+
+        $qbr = $this->modelManager->getRepository(Tag::class)
+                                  ->createQueryBuilder('t')->addSelect('relations.relationId');
+        $qbr->leftJoin('t.relations', 'relations')
+            ->leftJoin('relations.tableRegistry', 'tableRegistry')
+            ->andWhere(
+                $qbr->expr()->in('relations.relationId', ':ids'),
+                $qbr->expr()->eq('tableRegistry.tableName',
+                    $qbr->expr()->literal($this->relationCollector->getByAlias($relation)->getTableName())
+                )
+            )->setParameter('ids', $ids);
+
+        foreach ($qbr->getQuery()->getArrayResult() as $item) {
+            $relationId = $item['relationId'];
+            unset($item['relationId']);
+            // TODO: This way is not the nicest one, please find a better solution.
+            // @see custom/plugins/NetiTags/Service/Tag/Relations/Article.php:331 and so on
+            self::$tagsCache[$relation][$relationId][] = [
+                'id' => $item[0]['id'],
+                'title' => $item[0]['title'],
+            ];
+        }
     }
 }
